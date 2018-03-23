60695f53edfe69b207948979b5eebc9087372c35
Markus Bader <baderm@gonicus.de>
2018-03-23 14:46:06 +0100

    Initial commit - project skeleton

---
+++ source/class/flat/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("flat.theme.Appearance",
+{
+  extend : qx.theme.indigo.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ source/class/flat/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("flat.theme.Theme",
+{
+  meta :
+  {
+    color : flat.theme.Color,
+    decoration : flat.theme.Decoration,
+    font : flat.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : flat.theme.Appearance
+  }
+});
