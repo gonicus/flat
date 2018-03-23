ee1d0f5b9787f65303448cc76c718f8b084d1c3b
Markus Bader <baderm@gonicus.de>
2018-03-23 15:28:54 +0100

    Copied colors and fonts

--- source/class/flat/theme/flat/Font.js
+++ source/class/flat/theme/flat/Font.js
@@ -12,5 +12,44 @@ qx.Theme.define('flat.theme.flat.Font', {
   extend: qx.theme.indigo.Font,
 
   fonts: {
+    'default': {
+      size: 14,
+      family: ['Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+      sources: [
+        {
+          family: 'Open Sans',
+          source: [
+            'flat/fonts/OpenSans-Regular.ttf'
+          ]
+        }
+      ]
+    },
+
+    'small': {
+      size: 12,
+      family: ['Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+      sources: [
+        {
+          family: 'Open Sans',
+          source: [
+            'flat/fonts/OpenSans-Regular.ttf'
+          ]
+        }
+      ]
+    },
+
+    'bold': {
+      size: 14,
+      bold: true,
+      family: ['Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+      sources: [
+        {
+          family: 'Open Sans',
+          source: [
+            'flat/fonts/OpenSans-Bold.ttf'
+          ]
+        }
+      ]
+    }
   }
 })
